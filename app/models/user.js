b9c84ef16263474572aa141e70dda73de04be91a
sunilpolaki <136548188+sunilpolaki@users.noreply.github.com>
2025-06-02 13:47:02 +0530

    Update user.js

--- app/models/user.js
+++ app/models/user.js
@@ -1,8 +1,10 @@
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true }
 });
 
-module.exports = mongoose.model('User', userSchema);
+const User = mongoose.model('User', userSchema);
+export default User;
+
